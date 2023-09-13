761915bba535bcb76fd02bbff0e43c7948a5879c
juni0509 <junhee040509@naver.com>
2023-09-13 15:46:15 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,13 @@
 def print_hello():
     print("Techmo is god!!Techmo is god!!Techmo is god!!")
 
+def add(x, y):
+    a =x+y
+    return a
+
+def print_KIT():
+    print("KIT")
+
 if __name__ == '__main__':
-    print_hello()
-    
+        print_KIT()
+        print( add(1,2) )
